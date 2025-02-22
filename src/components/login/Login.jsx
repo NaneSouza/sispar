e46302734fe3d1c4fa2b3eb8d7cba615b2b654c4
NaneSouza <elainen19@gmail.com>
2025-02-21 21:17:06 -0300

    mensagem explicativa sobre a mudança

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss";
+
+function Login() {
+  const navigate = useNavigate(); //Iniciando o hook useNavigate
+
+  const irParaReembolsos = () => {
+    navigate("/reembolsos"); //Redirecionando para a página de reembolsos
+  };
+
+  return (
+    <main className={styles.mainLogin}>
+      <section className={styles.containerFoto}>
+      </section>
+
+      <section className={styles.formWapper}>
+        <div className={styles.boxLogo}>
+          <img src={Logo} alt="Logo da wilson sons" />
+          <h1>Boas vindas ao Novo Portal SISPAR </h1>
+          <p>Sistema de Emissão de Boletos e Parcelamento</p>
+        </div>
+
+        <form action="">
+          <input type="email" name="email" id="email" placeholder="Email" />
+
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+          />
+
+          <p>Esqueci minha senha</p>
+
+          <div className={styles.boxButton}>
+            <button onClick={irParaReembolsos}>Entrar</button>
+            <button>Criar conta</button>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+}
+export default Login;
---
+++ src/components/solicitacao/Solicitacao.jsx
@@ -0,0 +1,249 @@
+import NavBar from "../navbar/NavBar.jsx";
+import styles from "./Solicitacao.module.scss";
+import Home from "../../assets/Dashboard/home header.png";
+import Seta from "../../assets/Dashboard/Vector.png";
+import Deletar from "../../assets/Solicitacao/deletar.png";
+import Lixeira from "../../assets/Solicitacao/lixeira.png";
+import Motivo from "../../assets/Solicitacao/motivo.png"
+function Solicitacao() {
+  return (
+    <div className={styles.layoutSolicitacao}>
+      <NavBar />
+
+      <div className={styles.containerPrincipalSolicitacao}>
+        <header className={styles.headerSolicitacao}>
+          <img src={Home} alt="Vetor da casinha" />
+          <img src={Seta} alt="Vetor da setinha" />
+          <p> Reembolsos</p>
+          <img src={Seta} alt="Vetor da setinha" />
+          <p>Solicitação de Reembolsos</p>
+        </header>
+
+        <main className={styles.mainSolicitacao}>
+          <form className={styles.formSolicitacao}>
+            <div className={styles.grupo1}>
+              <div className={styles.inputNome}>
+                <label htmlFor="">Nome Completo</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputEmpresa}>
+                <label htmlFor="">Empresa</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputPrestacao}>
+                <label htmlFor="">Nº Prest. Contas</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputMotivo}>
+                <label htmlFor=""> Descrição / Motivo do Reembolso </label>
+                <textarea name="" id="">
+                  {" "}
+                </textarea>
+              </div>
+            </div>
+
+            <div className={styles.barraVertical}></div>
+
+            <div className={styles.grupo2}>
+              <div className={styles.inputData}>
+                <label htmlFor="">Data</label>
+                <input type="date" name="" id="" />
+              </div>
+
+              <div className={styles.tipoDeDespesas}>
+                <label htmlFor=""> Tipo de Despesa </label>
+
+                <select name="" id="">
+                  <option value="">Selecionar</option>
+                  <option value=""> Alimentação </option>
+                  <option value=""> Combustível</option>
+                  <option value=""> Condução</option>
+                  <option value=""> Estacionamento</option>
+                  <option value=""> Viagem Admin.</option>
+                  <option value=""> Viagem Operacional</option>
+                  <option value=""> Eventos de representação</option>
+                </select>
+              </div>
+
+              <div className={styles.centroDeCusto}>
+                <label htmlFor=""> Centro de Custo </label>
+
+                <select name="" id="">
+                  <option value=""> Selecionar</option>
+                  <option value="">
+                    1100109002 - FIN CONTROLES INTERNOS MTZ
+                  </option>
+                  <option value="">
+                    1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ
+                  </option>
+                  <option value="">1100110101 - FIN CONTABILIDADE MTZ</option>
+                </select>
+              </div>
+
+              <div className={styles.ordem}>
+                <label> Ord. Int.</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.divisoes}>
+                <label> Div.</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.pep}>
+                <label> PEP</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.moeda}>
+                <label htmlFor=""> Moeda</label>
+                <select name="" id="">
+                  <option value="">Selecionar</option>
+                  <option value="">BRL</option>
+                  <option value="">ARS</option>
+                  <option value="">USD</option>
+                </select>
+              </div>
+
+              <div className={styles.distancia}>
+                <label htmlFor=""> Dist / Km</label>
+                <input type="text" name="" id="" />
+              </div>
+
+              <div className={styles.valorKm}>
+                <label> Valor / Km</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.valorFaturado}>
+                <label> Val. Faturado</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.despesa}>
+                <label> Despesa</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.botoes}>
+                <button className={styles.botaoSalvar}> + Salvar</button>
+                <button className={styles.botaoDeletar}>
+                  {" "}
+                  <img src={Deletar} alt="" />
+                </button>
+              </div>
+            </div>
+          </form>
+
+         
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Colaborador(a)</th>
+                <th>Empresa</th>
+                <th>Nº Prest.</th>
+                <th>Data</th>
+                <th>Motivo</th>
+                <th>Tipo de despesa</th>
+                <th>Ctr. Custo</th>
+                <th>Ord. Int.</th>
+                <th>Div.</th>
+                <th>PEP</th>
+                <th>Moeda</th>
+                <th>Dist. Km</th>
+                <th>Val. Km</th>
+                <th>Val. Faturado</th>
+                <th>Despesa</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td> <img src={Lixeira} alt="ícone da lixeira" /></td>
+                <td>Samuel Prata</td>
+                <td>VnW</td>
+                <td>10</td>
+                <td>12/08/23</td>
+                <td> <img src={Motivo} alt="ícone do motivo" /></td>
+                <td>Gosta muito de pagode </td>
+                <td>Tomar muita água</td>
+                <td>200</td>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>BRL</td>
+                <td>12</td>
+                <td>20</td>
+                <td>300</td>
+              </tr>
+
+              <tr>
+                <td> <img src={Lixeira} alt="ícone da lixeira" /></td>
+                <td>Karinne</td>
+                <td>VnW</td>
+                <td>5</td>
+                <td>10/03/24</td>
+                <td> <img src={Motivo} alt="ícone do motivo" /></td>
+                <td>Alunas do VnW </td>
+                <td>Ser Mãezona</td>
+                <td>100</td>
+                <td>4</td>
+                <td>3</td>
+                <td>1</td>
+                <td>BRL</td>
+                <td>10</td>
+                <td>30</td>
+                <td>200</td>
+              </tr>
+
+              <tr>
+                <td> <img src={Lixeira} alt="ícone da lixeira" /></td>
+                <td>João</td>
+                <td>VnW</td>
+                <td>12</td>
+                <td>01/04/23</td>
+                <td> <img src={Motivo} alt="ícone do motivo" /></td>
+                <td>Consulta Otorrino</td>
+                <td>Cumprimentar acima dB adequados </td>
+                <td>500</td>
+                <td>5</td>
+                <td>3</td>
+                <td>8</td>
+                <td>BRL</td>
+                <td>14</td>
+                <td>50</td>
+                <td>100</td>
+              </tr>
+
+              <tr>
+                <td> <img src={Lixeira} alt="ícone da lixeira" /></td>
+                <td>Joy</td>
+                <td>VnW</td>
+                <td>6</td>
+                <td>14/02/22</td>
+                <td> <img src={Motivo} alt="ícone do motivo" /></td>
+                <td>Alunos do VnW </td>
+                <td>Aconselhar</td>
+                <td>300</td>
+                <td>3</td>
+                <td>1</td>
+                <td>2</td>
+                <td>BRL</td>
+                <td>13</td>
+                <td>40</td>
+                <td>400</td>
+              </tr>
+
+            </tbody>
+
+          </table>
+        </main>
+      </div>
+    </div>
+  );
+}
+export default Solicitacao;
